503f3a3cd85cf9bb5bb0e14355bb1e31ba88378d
Heng Hakley <Hakleybtb18kh@gmail.com>
2026-04-20 22:07:09 +0700

    B015 - US1 - dto for pass data

--- lib/data/dtos/ride_pass_dto.dart
+++ lib/data/dtos/ride_pass_dto.dart
@@ -1,1 +1,37 @@
+import '../../models/pass_type.dart';
+import '../../models/ride_pass.dart';
 
+class RidePassDto {
+  const RidePassDto({required this.typeName, required this.purchasedAtIso});
+
+  final String typeName;
+  final String purchasedAtIso;
+
+  factory RidePassDto.fromDomain(RidePass pass) {
+    return RidePassDto(
+      typeName: pass.type.name,
+      purchasedAtIso: pass.purchasedAt.toIso8601String(),
+    );
+  }
+
+  factory RidePassDto.fromMap(Map<Object?, Object?> source) {
+    return RidePassDto(
+      typeName: (source['typeName'] ?? '').toString(),
+      purchasedAtIso: (source['purchasedAtIso'] ?? '').toString(),
+    );
+  }
+
+  RidePass toDomain() {
+    return RidePass(
+      type: PassType.values.firstWhere(
+        (item) => item.name == typeName,
+        orElse: () => PassType.day,
+      ),
+      purchasedAt: DateTime.parse(purchasedAtIso),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'typeName': typeName, 'purchasedAtIso': purchasedAtIso};
+  }
+}
