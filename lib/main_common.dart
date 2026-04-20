efb9a5e2e7f92687d3bd10e27959d2fbd9765aa7
Vichea-collab <luchvichea43@gmail.com>
2026-04-20 21:35:52 +0700

    B008 - US4 - implement the booking dto and repo

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'app.dart';
 import 'data/repositories/ride_repository.dart';
+import 'ui/theme/app_theme.dart';
+import 'ui/viewmodels/ride_app_view_model.dart';
+import 'ui/views/screens/app_shell_screen.dart';
 
 Future<void> mainCommon({
   required Future<RideRepository> Function() createRepository,
@@ -9,4 +12,27 @@ Future<void> mainCommon({
   WidgetsFlutterBinding.ensureInitialized();
   final repository = await createRepository();
   runApp(RideRentalApp(repository: repository));
+}
+
+class RideRentalApp extends StatelessWidget {
+  const RideRentalApp({super.key, required this.repository});
+
+  final RideRepository repository;
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<RideAppViewModel>(
+      create: (_) {
+        final viewModel = RideAppViewModel(repository: repository);
+        viewModel.initialize();
+        return viewModel;
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'RideFlow',
+        theme: AppTheme.light(),
+        home: const AppShellScreen(),
+      ),
+    );
+  }
 }
