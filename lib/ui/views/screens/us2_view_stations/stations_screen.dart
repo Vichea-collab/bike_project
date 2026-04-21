72b1e427c85a0603dd4a0baa0ad2bbc207ca2a25
Chhoeun Venghourt <hourt2133@gmail.com>
2026-04-21 14:41:19 +0700

    "B017 - US2 - Create stations screen and connect to view model"

--- lib/ui/views/screens/us2_view_stations/stations_screen.dart
+++ lib/ui/views/screens/us2_view_stations/stations_screen.dart
@@ -1,1 +1,48 @@
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../../models/bike_station.dart';
+import '../../../viewmodels/ride_app_view_model.dart';
+import 'view_model/stations_view_model.dart';
+import 'widgets/stations_content.dart';
+
+class StationsScreen extends StatefulWidget {
+  const StationsScreen({super.key, required this.onOpenBikes});
+
+  final ValueChanged<BikeStation> onOpenBikes;
+
+  @override
+  State<StationsScreen> createState() => _StationsScreenState();
+}
+
+class _StationsScreenState extends State<StationsScreen> {
+  late final StationsViewModel _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = StationsViewModel(
+      appViewModel: context.read<RideAppViewModel>(),
+    );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _viewModel,
+      builder: (context, _) {
+        return StationsContent(
+          viewModel: _viewModel,
+          onOpenBikes: widget.onOpenBikes,
+        );
+      },
+    );
+  }
+}
